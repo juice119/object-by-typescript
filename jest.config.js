b9b82a474860477d5fcd1697a4bda51d7dee1c28
Juice <3221ju@gmail.com>
2021-12-03 04:06:36 +0900

    project init

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = async () => {
+  return {
+    verbose: true,
+    testMatch: [
+        "**/test/**/*.test.ts"
+    ],
+  };
+};
---
+++ src/0.hello/test/hello.test.ts
@@ -0,0 +1,7 @@
+import {hello} from "../src/hello";
+
+describe('0.0.hello test를 수행합니다.', () => {
+  it('hello함수는 "0.0.hello"를 응답합니다.', () => {
+    expect(hello()).toBe('hello');
+  });
+});
