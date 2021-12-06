16d5e5857e326947107f261c4ea6376a29694196
Juice <3221ju@gmail.com>
2021-12-07 07:14:38 +0900

    [chapter2] 주석 제거

--- src/chapter02/src/movie/step01/pricing/NoneDiscountPolicy.ts
+++ src/chapter02/src/movie/step01/pricing/NoneDiscountPolicy.ts
@@ -3,7 +3,6 @@ import { Money } from '../../../money/Money';
 import { Screening } from '../Screening';
 
 export class NoneDiscountPolicy extends DiscountPolicy {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   protected getDiscountAmount(screening: Screening): Money {
     return Money.ZERO;
   }
